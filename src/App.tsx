83e1742f91e65804daad1049b928dfacd8e5b838
Ademilson Luiz de Sousa <ademilsodream@gmail.com>
2025-06-18 08:41:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
-import { OptimizedAuthProvider } from '@/contexts/OptimizedAuthContext';
+import { AuthProvider } from '@/contexts/OptimizedAuthContext';
 import { CurrencyProvider } from '@/contexts/CurrencyContext';
 import Login from '@/pages/Login';
 import EmployeeLayout from '@/components/EmployeeLayout';
@@ -12,18 +12,18 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <OptimizedAuthProvider>
+      <AuthProvider>
         <CurrencyProvider>
           <Router>
             <Routes>
               <Route path="/login" element={<Login />} />
-              <Route path="/employee" element={<EmployeeLayout><Outlet /></EmployeeLayout>} />
+              <Route path="/employee/*" element={<EmployeeLayout />} />
               <Route path="/" element={<Login />} />
             </Routes>
           </Router>
           <Toaster />
         </CurrencyProvider>
-      </OptimizedAuthProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
